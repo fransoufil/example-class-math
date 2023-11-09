c69ce4ab24cc90686b9d242bf576809e6e485f4d
fransoufil <fran.sf@uol.com.br>
2023-11-09 16:08:14 -0300

    Frist commit

---
+++ src/main/ClasseMathExamples.java
@@ -0,0 +1,32 @@
+package main;
+
+public class ClasseMathExamples {
+
+	public static void main(String[] args) {
+		
+		  //Retorno do seno do angulo especificado
+		    System.out.printf("Resultado do método sin(): %.2f\n", Math.sin(10.5));
+		    
+		  //Retorno do cosseno do angulo especificado
+		    System.out.printf("Resultado do método cos(): %.2f\n", Math.cos(10.5));
+		    
+		  //Retorno da tangente do angulo especificado
+		    System.out.printf("Resultado do método tan(): %.2f\n", Math.tan(10.5));
+		    
+		  //Retorna o maior número inteiro menor ou igual ao valor
+		    System.out.printf("Resultado do método floor(): %.2f\n", Math.floor(10.5));
+		    
+		  //Retorna o menor número inteiro maior ou igual ao valor
+		    System.out.printf("Resultado do método ceil(): %.2f\n", Math.ceil(10.5));
+		    
+		  //Retorna o valor absoluto
+		    System.out.printf("Resultado do método abs(): %.2f\n", Math.abs(-10.5));
+		    
+		  //Encontra a raiz quadrada
+		    System.out.printf("Resultado do método sqrt(): %.2f\n", Math.sqrt(81));
+		    
+		  //Eleva "a" à potência de "b"
+		    System.out.printf("Resultado do método pow(): %.2f", Math.pow(2, 5));
+	}
+
+}
